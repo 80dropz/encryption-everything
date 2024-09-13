bce800cc22cbf602a22c91bafd76e01d57ec3b86
80dropz <153262187+80dropz@users.noreply.github.com>
2024-09-13 15:04:49 -0400

    Rewrote all of encrypted.py
    This should be almost perfect will add more things l8tr

--- Encrypter.py
+++ Encrypter.py
@@ -1,47 +1,116 @@
-import cryptography
+
 from cryptography.fernet import Fernet
 import customtkinter
 from customtkinter import filedialog
-import tkinter
 
-key = Fernet.generate_key()
-print(key)
-k = Fernet(key)
+configfound = False
+string = "Fillertxt"
 
-def string_changed(*args):
+def configlook():
+    global key
+    global configfound
+    try: 
+        with open("key.key", "rb") as f:
+            keyy = f.read()
+            key = Fernet(keyy)
+            configfound = not False
+            configlbl.configure(text="Config found", text_color="green")
+    except:
+        configlbl.configure(text="Config Generated", text_color="Green")
+        configfound = not True
+        keyy = Fernet.generate_key()
+        key = Fernet(keyy)
+        with open("key.key", "wb") as f:
+            f.write(key)
+
+
+def fileenc():
+    filepath = filedialog.askopenfilename()
+    if filepath.endswith(".txt", ".py", "html", "css", "js", "json"):
+        with open(filepath, "r") as f:
+            filecontents = f.read()
+            print(filecontents)
+            fileencfinished = key.encrypt(filecontents.encode())
+            with open("file_encrypted.txt", "wb") as f:
+                f.write(fileencfinished)
+                writtenlbl.configure(text="Written to file_encrypted.txt", text_color="green")
+    else:
+        writtenlbl.configure(text="File type not supported", text_color="red")
+
+def encyrpt():
+    if string == "Fillertxt":
+        writtenlbl.configure(text="No text entered", text_color="red")
+    else:
+        writtenlbl.configure(text="Written to Encrypted.txt", text_color="green")
+        with open("Encrypted.txt", "wb") as f:
+            encyrpttxt = key.encrypt(string.encode())
+            f.write(encyrpttxt)
+
+def decrypt():
+    filename = filedialog.askopenfilename()
+    if filename.endswith(".txt"):
+        with open(filename, "rb") as f:
+            filecontents = f.read()
+            decryptfinished = key.decrypt(filecontents)
+            with open("decrypted.txt", "wb") as f:
+                f.write(decryptfinished)
+                writtenlbl.configure(text="Written to decrypted.txt", text_color="green")
+def ent_changed(*args):
     global string
-    string = encryptentry.get()
+    string = encyrptent.get()
     currenttext = f" current text: {string}"
     print(currenttext)
-    lbltoencrypt.configure(text=string)
 
-def encrypt():
-    encstring = k.encrypt(string.encode())
-    encstringfile = str(encstring)
-    with open("encrypted.txt", "w") as f:
-        f.write(encstringfile + "\n")
-        f.write(f"Key to unencrypt: {key}")
-mainwindow = customtkinter.CTk()
-mainwindow.geometry("500x500")
-mainwindow.title("An attempted encryption")
+try:
+    with open("key.key", "rb") as f:
+        key = f.read()
+except:
+    pass
+
+main = customtkinter.CTk()
+main.geometry("500x500")
+main.title("Encryptor and Decryptor")
+
+header = customtkinter.CTkLabel(main, text="80dropz Encryptor", font=("Arial", 20))
+header.place(relx=.5, rely=.2, anchor="center")
+
+entrychanged = customtkinter.StringVar()
+encyrptent = customtkinter.CTkEntry(main, placeholder_text=" Encrypt", textvariable=entrychanged)
+encyrptent.place(relx=.5, rely=.4, anchor="center")
+entrychanged.trace_add("write", ent_changed)
+
+fileencbtn = customtkinter.CTkButton(main, text="Select File", command=fileenc, fg_color="dark green")
+fileencbtn.place(relx=.3, rely=.6, anchor="center")
+
+encrpybtn = customtkinter.CTkButton(main, text="Encrypt", command=encyrpt, fg_color="Green")
+encrpybtn.place(relx=.3, rely=.5, anchor="center")
+
+decryptbtn = customtkinter.CTkButton(main, text="Decrypt", command=decrypt, fg_color="Red")
+decryptbtn.place(relx=.7, rely=.5, anchor="center")
+
+configlbl = customtkinter.CTkLabel(main, text=" ", text_color="green", font=("Arial", 14))
+configlbl.place(relx=.2, rely=.05, anchor="center")
+
+writtenlbl = customtkinter.CTkLabel(main, text=" ", text_color="green", font=("Arial", 14))
+writtenlbl.place(relx=.5, rely=.93, anchor="center")
+if __name__ == "__main__":
+    configlook()
+
+main.mainloop()
 
 
-header = customtkinter.CTkLabel(master=mainwindow, text="An attempted encryption", font=("Arial", 20))
-header.place(relx=.5, rely=.05, anchor="center")
-subheader = customtkinter.CTkLabel(master=mainwindow, text="By: @80dropz", font=("Arial", 14))
-subheader.place(relx=.5, rely=.1, anchor="center")
-
-encryptbtn = customtkinter.CTkButton(master=mainwindow, text="Encrypt", command=encrypt)
-encryptbtn.place(relx=.5, rely=.5, anchor="center")
-
-encvar = customtkinter.StringVar()
-encryptentry = customtkinter.CTkEntry(mainwindow, placeholder_text="Enter string to encrypt", width=150, height=50, textvariable=encvar)
-encryptentry.place(relx=.5, rely=.6, anchor="center")
-encvar.trace_add("write", string_changed)
-
-lbltoencrypt = customtkinter.CTkLabel(master=mainwindow, text="Enter string to encrypt")
-lbltoencrypt.place(relx=.5, rely=.8, anchor="center")
-
-errorlbl = customtkinter.CTkLabel(master=mainwindow, text=" ", text_color="red")
-errorlbl.place(relx=.5, rely=.7, anchor="center")
-mainwindow.mainloop()
+def configlook():
+    global key
+    global configfound
+    try: 
+        with open("key.key", "rb") as f:
+            keyy = f.read()
+            key = Fernet(keyy)
+            configfound = not False
+            configlbl.configure(text="Config found", text_color="green")
+    except:
+        configlbl.configure(text="Config Generated", text_color="Green")
+        configfound = not True
+        key = Fernet.generate_key()
+        with open("key.key", "wb") as f:
+            f.write(key)
